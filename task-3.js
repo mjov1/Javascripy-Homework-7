9a03637edd4bb90885129b3c398d3f26ea69ef5f
Martin Jovanovski <martinjovanovski@Martins-MacBook-Air.local>
2023-03-22 00:14:25 +0100

    task-3

---
+++ task-3.js
@@ -0,0 +1,12 @@
+
+
+function reverseArrey(arr) {
+    return arr.reverse();
+}
+
+
+let a = reverseArrey([1, 2, 3, 4]);
+console.log(a);
+
+let b = reverseArrey([90, 80, 70, 60, 50,]);
+console.log(b);
